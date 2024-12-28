257ee454dd3b4600e4358a7edcbba2b57f7a2251
suryaprakash.pisay <suryaprakash.pisay@live.com>
2024-12-28 19:14:57 +0530

    adding codes paces configuration
    #installing docker by default in the dev container

--- .devcontainer/docker-install.bash
+++ .devcontainer/docker-install.bash
@@ -1,9 +1,9 @@
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt remove $pkg; done
 
 # Add Docker's official GPG key:
-apt-get update
+apt update
 
-apt-get install ca-certificates curl
+apt install ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
